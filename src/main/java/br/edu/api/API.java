e75eac8484fe2017054aee8beb1529a7cc43d847
joaowesley5678 <joaowesley567@gmail.com>
2024-09-17 15:10:57 -0300

    added first api request

---
+++ src/main/java/br/edu/api/API.java
@@ -0,0 +1,14 @@
+package br.edu.api;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Aluno
+ */
+public class API {
+    
+}
