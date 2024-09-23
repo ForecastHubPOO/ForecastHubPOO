959e3517a1bcdfccb0c3e6438967f46b9d5e6dcb
PdroBarbosaIF <pedro.barbosa40@aluno.ifce.edu.br>
2024-09-23 18:47:16 -0300

    slkchr

--- src/main/java/com/mycompany/forecasthub/MudarParaTelaDePrevisao.java
+++ src/main/java/com/mycompany/forecasthub/MudarParaTelaDePrevisao.java
@@ -1,12 +1,23 @@
 package com.mycompany.forecasthub;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class MudarParaTelaDePrevisao {
 
+    
+    @FXML
+    private TextField myTextField;
+    
+    String pesquisa;
+    
+    
     @FXML
     private void mudarParaTelaDePrevisao() throws IOException {
+        pesquisa = myTextField.getText();
+        System.out.println(pesquisa);
         App.setRoot("TelaDePrevisao");
     }
 }
